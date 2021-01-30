43b47a87d1902c80b8b22b9ab7ae221145cade67
Caio Batista <caio.batista@gmail.com>
2021-01-30 12:04:18 -0800

    d4

--- game.py
+++ game.py
@@ -202,7 +202,7 @@ def online_create():
 
         return redirect(url_for('online_home'))
     
-    return request.referrer, request.host_url
+    return str(request.referrer, request.host_url)
     abort(401)
 
 
