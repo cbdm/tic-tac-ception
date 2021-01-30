9b9e0308832fc423a669747fa8f40a55ba4873b9
Caio Batista <caio.batista@gmail.com>
2021-01-30 13:10:26 -0800

    Migrate code style to Black

--- ai_options.py
+++ ai_options.py
@@ -1,19 +1,20 @@
-'''ai_options.py
+"""ai_options.py
 
 Author: Caio Batista de Melo
 Date Created: 2020-12-28
 Date Modified: 2020-12-28
 Description: Defines the different types of AI available.
-'''
+"""
 
 from random import choice
 from bigboard import BigBoard
+
 
 class Random:
     def choose_best_move(board):
         valid_moves = board.get_valid_moves()
         small_board = choice(list(valid_moves))
-        row, col = int(small_board)//3, int(small_board)%3
+        row, col = int(small_board) // 3, int(small_board) % 3
         x, y = choice(valid_moves[small_board])
         return row, col, x, y
 
--- smallboard.py
+++ smallboard.py
@@ -1,18 +1,18 @@
-'''smallboard.py
+"""smallboard.py
 
 Author: Caio Batista de Melo
 Date Created: 2020-11-06
 Last Modified: 2020-11-07
 Description: Implements a class to keep track of a basic tic-tac-toe game.
-'''
+"""
 
 from itertools import chain
+
 
 class SmallBoard(object):
     def __init__(self):
         self._board = [[None for _ in range(3)] for _ in range(3)]
-        self._winner = None    
-
+        self._winner = None
 
     def make_move(self, x, y, player):
         assert x < len(self._board) and y < len(self._board)
@@ -25,18 +25,23 @@ class SmallBoard(object):
         else:
             return False
 
-
     def check_winner(self):
         # Check if we already know the winner.
-        if self._winner is not None: return self._winner
-        
+        if self._winner is not None:
+            return self._winner
+
         # Finds all plays for each row.
-        rows = (set(row) for row in self._board) 
+        rows = (set(row) for row in self._board)
         # Finds all plays for each column.
-        columns = ({self._board[j][i] for j in range(len(self._board))} for i in range(len(self._board)))
+        columns = (
+            {self._board[j][i] for j in range(len(self._board))}
+            for i in range(len(self._board))
+        )
         # Finds all plays in the main and reverse diagonals.
-        diagonals = ({self._board[i][i] for i in range(len(self._board))}, # main diagonal
-                     {self._board[i][-1-i] for i in range(len(self._board))}) # reverse diagonal
+        diagonals = (
+            {self._board[i][i] for i in range(len(self._board))},  # main diagonal
+            {self._board[i][-1 - i] for i in range(len(self._board))},
+        )  # reverse diagonal
 
         for m in chain(rows, columns, diagonals):
             # Check if the same player has taken all positions in this row/col/diag.
@@ -46,18 +51,21 @@ class SmallBoard(object):
 
         return None
 
-
     def is_over(self):
         # Check if the game is over (no more open positions or there's a winner).
-        return all((None not in row for row in self._board)) or self.check_winner() is not None
-
+        return (
+            all((None not in row for row in self._board))
+            or self.check_winner() is not None
+        )
 
     def get_empty(self):
         # Return a list with the empty positions in the board.
-        return [(i, j) for i in range(len(self._board))
-                       for j in range(len(self._board))
-                       if self._board[i][j] is None]
-
+        return [
+            (i, j)
+            for i in range(len(self._board))
+            for j in range(len(self._board))
+            if self._board[i][j] is None
+        ]
 
     def get_board(self):
         return self._board
